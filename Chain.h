7264b976b78e3682c358a8b61fdafb504d19c38a
Mary T <79993417+maroulaki@users.noreply.github.com>
2022-05-05 23:34:34 +0300

    Add files via upload

---
+++ Chain.h
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <random>
+#include <functional>
+#include <exception>
+using namespace std;
+
+template<class T>
+class ChainNode {
+    friend Chain<T>;
+    private:
+        T data;
+        ChainNode<T>
+    *link;
+};
+
+template <class T>
+class Chain {
+    public:
+        Chain() {first = 0;}
+        ~Chain();
+
+        bool IsEmpty() const;
+
+        int Length() const;
+
+        bool Find(int k, T& x) const;
+
+        int Search(const T& x) const;
+
+        Chain<T> & Delete(int k, T& x);
+
+        Chain<T> & Insert(int k, const T& x);
+
+        void Output(iostream& out) const;
+
+    private:
+        ChainNode<T> *first; // pointer to first node
+};
+
+
+
---
+++ Main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <Chain.h>
+using namespace std;
+
+template<class T>
+bool Chain<T>::IsEmpty() const {
+    return first == 0;
+}
+
+template<class T>
+int Chain<T>::Length() const {
+    if (first == 0) {
+        return 0;
+    } else {
+        ChainNode<T> *current = first;
+        int len=1;
+        while (current->link != NULL) {
+            len++;
+            current = current->link;
+        }
+        return len;
+    }
+}
+
+template<class T>
+bool Chain<T>::Find(int k, T &x) const {
+    if (k<1) { return false; }
+    else {
+        ChainNode<T> *current = first;
+        int index=1;
+        while ((index < k)&&(current->link != NULL)) {
+            current = current->link;
+            index++;
+        }
+        if (current) {
+            x = current->data; 
+            return true;
+        }
+        return false; // no k'th element
+            }
+
+
+}
+
+
